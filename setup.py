fadadcd3ed7c5bf7a2feecaad280d225d8e1bc8b
budel <daniel.budelmann@googlemail.com>
2024-10-31 21:40:45 +0100

    cythonizing all in one go

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name='Segmentation Example v6',
-    ext_modules=cythonize("segmentation_v6.pyx"),
+    name='Segmentation Examples',
+    ext_modules=cythonize([
+        "segmentation_v2.pyx",
+        "segmentation_v2_without_numpy.pyx",
+        "segmentation_v3.pyx",
+        "segmentation_v4.pyx",
+        "segmentation_v5.pyx",
+        "segmentation_v6.pyx",
+    ], annotate=True),
 )
 

05fc5f0046f342a3d142538239e6924733d8709d
budel <daniel.budelmann@googlemail.com>
2024-11-02 08:36:03 +0100

    explicit inlining works with prange

--- setup.py
+++ setup.py
@@ -3,15 +3,9 @@ from Cython.Build import cythonize
 
 setup(
     name='Segmentation Examples',
-    ext_modules=cythonize([
-        "segmentation_v2.pyx",
-        "segmentation_v2_without_numpy.pyx",
-        "segmentation_v3.pyx",
-        "segmentation_v4.pyx",
-        "segmentation_v5.pyx",
-        "segmentation_v6.pyx",
-    ],
-    annotate=True,
+    ext_modules=cythonize(
+        ["*.pyx"],
+        annotate=True,
     )
 )
 
--- test_timings.py
+++ test_timings.py
@@ -13,26 +13,29 @@ from segmentation_v3 import runSequentialSegmentation as runSegmentation3
 from segmentation_v4 import runSequentialSegmentation as runSegmentation4
 from segmentation_v5 import runSequentialSegmentation as runSegmentation5
 from segmentation_v6 import runSequentialSegmentation as runSegmentation6
+from segmentation_v7 import runSequentialSegmentation as runSegmentation7
 
 cluster_n = 3
-N = 1000
+N = 10000
 img = np.random.randint(256, size=(N, N, 3), dtype=np.uint8)
 img = np.ascontiguousarray(img)
 means = np.random.randint(256, size=(cluster_n, 3)).astype(np.float64)
 means = np.ascontiguousarray(means)
 '''
-    it, t = timeit.Timer('runSequentialSegmentation(img, means)', setup=s).autorange()
-    print(f"Naive per run: {t/it}")
-    it, t = timeit.Timer('runSegmentation1(img, means)', setup=s).autorange()
-    print(f"v1 per run: {t/it}")
-    it, t = timeit.Timer('runSegmentation2(img, means)', setup=s).autorange()
-    print(f"v2 per run: {t/it}")
+#    it, t = timeit.Timer('runSequentialSegmentation(img, means)', setup=s).autorange()
+#    print(f"Naive per run: {t/it}")
+#    it, t = timeit.Timer('runSegmentation1(img, means)', setup=s).autorange()
+#    print(f"v1 per run: {t/it}")
+#    it, t = timeit.Timer('runSegmentation2(img, means)', setup=s).autorange()
+#    print(f"v2 per run: {t/it}")
     it, t = timeit.Timer('runSegmentation3(img, means)', setup=s).autorange()
     print(f"v3 per run: {t/it}")
     it, t = timeit.Timer('runSegmentation4(img, means)', setup=s).autorange()
     print(f"v4 per run: {t/it}")
-    it, t = timeit.Timer('runSegmentation5(img, means)', setup=s).autorange()
-    print(f"v5 per run: {t/it}")
-    it, t = timeit.Timer('runSegmentation6(img, means)', setup=s).autorange()
-    print(f"v6 per run: {t/it}")
+#    it, t = timeit.Timer('runSegmentation5(img, means)', setup=s).autorange()
+#    print(f"v5 per run: {t/it}")
+#    it, t = timeit.Timer('runSegmentation6(img, means)', setup=s).autorange()
+#    print(f"v6 per run: {t/it}")
+    it, t = timeit.Timer('runSegmentation7(img, means)', setup=s).autorange()
+    print(f"v7 per run: {t/it}")
 
